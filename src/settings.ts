48f2d3c5191d0335056a9370ba194cb7ced24958
Vincent Niehues <vniehues@icloud.com>
2022-01-13 21:58:02 +0100

    rename alias

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeLinakController';
+export const PLATFORM_NAME = 'LinakController';
 
 /**
  * This must match the name of your plugin as defined the package.json
