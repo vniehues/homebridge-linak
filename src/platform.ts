b6f7d108ae6f20562c1b34ea53fb199188931908
Vincent Niehues <vniehues@icloud.com>
2022-01-22 23:44:29 +0100

    added the ability to reconfigure cached accessories

--- src/platform.ts
+++ src/platform.ts
@@ -103,8 +103,8 @@ export class LinakDeskPlatform implements DynamicPlatformPlugin {
         this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
         // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+        existingAccessory.context.device = device;
+        this.api.updatePlatformAccessories([existingAccessory]);
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
